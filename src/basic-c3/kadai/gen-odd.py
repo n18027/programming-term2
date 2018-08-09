ce6ddb30d6b9625647cfe8257981f8dbf694b65a
n18027` <n18027@it-colloge.ac.jp>
2018-08-09 16:50:52 +0900

    zyugyo

---
+++ src/basic-c3/kadai/gen-odd.py
@@ -0,0 +1,10 @@
+def gen0dd():
+    i = 1
+    while i <= 30:
+        yield i
+        i += 2
+
+
+it = gen0dd()
+for v in it:
+    print(v, end=",")
---
+++ src/basic-c3/kadai/yield-test.py
@@ -0,0 +1,8 @@
+def genlto3()
+    yield 1:
+    yield 2:
+    yield 3:
+
+it = genlto3()
+for i in it:
+    print(i)
